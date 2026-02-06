ab67a534ad03ac614e6c69afd34d8a32177e696a
Ayham Alsalman <ayhammalsalman@gmail.com>
2026-02-06 06:25:57 +0300

    add a route to refresh the Access token using the Refresh token

--- app/api/endpoints.py
+++ app/api/endpoints.py
@@ -5,7 +5,7 @@ from db import *
 from crud import get_from_db, save_to_db, delete_from_db, get_all_products, get_user_by_email, get_user_by_username, is_email_exists, is_username_exists
 from crud import authenticate_user , raise_invalid_credentials
 from schemas import UserCreate, UserRead, UserLogin
-from core import hash_password, create_tokens
+from core import hash_password, create_tokens , create_access_token , decode_refresh_token
 
 router = APIRouter()
 
@@ -83,3 +83,14 @@ async def login(login_data:UserLogin, sec: Session = Depends(get_session)):
         "token_type": "bearer"
     }
 
+
+@router.post("/refresh")
+async def refresh_access_token(refresh_token: str):
+    user_id = decode_refresh_token(refresh_token)
+    if not user_id:
+        raise_invalid_credentials
+    new_token = refresh_access_token(user_id)
+    return {
+        "access_token": new_token,
+        "token_type": "bearer"
+    }
--- app/core.py
+++ app/core.py
@@ -1,6 +1,6 @@
 from passlib.context import CryptContext
 from datetime import datetime , timedelta, timezone
-from jose import jwt
+from jose import jwt, JWTError
 from config import settings
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto", bcrypt__truncate_error=True)
@@ -23,6 +23,21 @@ def create_tokens(id:int):
     return access_token, refresh_token
 
 
+def create_access_token(id:int):
+    access_expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = jwt.encode({"sub": str(id), "exp": access_expire}, SECRET_KEY, algorithm=ALGORITHM)
+    return access_token
+
 
 def verify_password(plain_password: str, hashed_password: str) ->bool:
     return pwd_context.verify(plain_password, hashed_password)
+
+def decode_refresh_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id: str = payload.get("sub")
+        if user_id is None:
+            return None
+        return int(user_id)
+    except JWTError:
+        return None
